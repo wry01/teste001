26685c8f07ae39e71a5f7607ecba55e748fa9bdd
Mickzada <davifeitoza137@gmail.com>
2025-01-25 17:54:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Exibe o modal assim que a página carregar
+window.onload = function() {
+    var modal = document.getElementById('termosModal');
+    modal.style.display = 'flex';
+    
+    // Quando o usuário clicar em "Concorda e Aceita", o modal será fechado
+    var concordaBtn = document.getElementById('concordaBtn');
+    concordaBtn.onclick = function() {
+        modal.style.display = 'none';
+    };
+};
 
